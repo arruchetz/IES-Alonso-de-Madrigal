bdaefd927e8ce150b54d54849dce1f73317e4953
arruchetz <daniel.gararr@educa.jcyl.es>
2023-02-08 15:29:57 +0100

    IES Alonso de Madrigal

---
+++ src/com/iesam/amadrigal/domain/models/Curso.java
@@ -0,0 +1,22 @@
+package com.iesam.amadrigal.domain.models;
+
+public class Curso extends Instituto{
+    private String nombre;
+    private String descripcion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
---
+++ src/com/iesam/amadrigal/domain/models/Profesor.java
@@ -0,0 +1,24 @@
+package com.iesam.amadrigal.domain.models;
+
+import java.util.Date;
+
+public class Profesor extends Persona {
+    private String tituloAcademico;
+    private Date fechaAlta;
+
+    public String getTituloAcademico() {
+        return tituloAcademico;
+    }
+
+    public void setTituloAcademico(String tituloAcademico) {
+        this.tituloAcademico = tituloAcademico;
+    }
+
+    public Date getFechaAlta() {
+        return fechaAlta;
+    }
+
+    public void setFechaAlta(Date fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
+}
